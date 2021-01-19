d6c376d82ac87fe02231f3398b13cceb63de41b4
GANESH CHANDRA <77651439+M-G-C-64@users.noreply.github.com>
2021-01-19 07:56:20 +0530

    Create SnakePattern.py

---
+++ SnakePattern.py
@@ -0,0 +1,16 @@
+num = int(input())
+i = 1
+k=1
+
+while k<num-1 :
+    j = 0
+    #humm = k
+    jumm = jumn = i+k
+    j = i
+    while jumm > j:
+        print(jumm-1,end = " ")
+        jumm = jumm - 1
+    print("\n", end = "")
+
+    i = jumn
+    k = k+1
